30aa25bbdd144506ce90918e2cc9f00d23e7fe5b
RevolverSnake412 <oussag@hotmail.fr>
2024-08-08 01:48:48 +0100

    Footer added

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+// Header.js
+import React, { useContext } from 'react';
+import FooterCSS from '../../assets/styles/Footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className={FooterCSS.footer}>
+      <footer>
+        stay tuned...
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
