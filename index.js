87ade31c8ed046039a00f24c2c984ca3d84e4695
RevolverSnake412 <oussag@hotmail.fr>
2024-08-12 01:31:13 +0100

    update

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(cors({
     credentials: true,
   }));
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
