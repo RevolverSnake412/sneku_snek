57fa55bd5bbe90de85ae0b736b2b37bdd632fbca
RevolverSnake412 <oussag@hotmail.fr>
2024-08-13 01:04:49 +0100

    Global changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes as R } from 'react-router-dom';
 import Header from './components/Layout/Header';
-import { AuthProvider } from './components/Layout/AuthContext';
+import { AuthProvider } from './services/AuthContext';
 import Home from './components/Auth/Home';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-import './App.css';
-import Footer from './components/Layout/Footer';
+import About from './components/Auth/About';
+import './App.css'
 
 function App() {
   return (
     <AuthProvider>
     <Router>
       <Header />
-        <R>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </R>
+        <div className='mainContent'>
+          <R>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/about" element={<About />} />
+          </R>
+        </div>
     </Router>
-    <Footer />
     </AuthProvider>
   );
 }
