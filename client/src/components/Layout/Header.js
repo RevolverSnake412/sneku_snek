6e3665314bc03a5f3e77477105391a710356d73d
RevolverSnake412 <oussag@hotmail.fr>
2024-08-13 01:04:55 +0100

    Update header

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,6 +1,6 @@
 // Header.js
 import React, { useContext } from 'react';
-import { AuthContext } from './AuthContext';
+import { AuthContext } from '../../services/AuthContext';
 import HeaderCSS from '../../assets/styles/Header.module.css'
 
 const Header = () => {
@@ -18,11 +18,15 @@ const Header = () => {
         <div><a href="/">Sneku Snek</a></div>
         {user ? (
           <div>
+            <a href="/">Home</a>
+            <a href="/about">About Us</a>
             <span>{user.username}</span>
             <button onClick={logout}>Logout</button>
           </div>
         ) : (
           <div className={HeaderCSS.right}>
+            <a href="/">Home</a>
+            <a href="/about">About Us</a>
             <a href="/login">Login</a>
             <a href="/register">Register</a>
           </div>
