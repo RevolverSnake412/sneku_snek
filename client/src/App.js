f408c41f8cbe5ef8dc08bf1286d5a567256b5ffa
RevolverSnake412 <oussag@hotmail.fr>
2024-08-19 18:13:15 +0100

    404 page implemented

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Auth/Home';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import About from './components/Auth/About';
+import NotFound from './components/Layout/404';
 import './App.css'
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
           </R>
         </div>
     </Router>
